b0fe2a6cd035f219a820d73a80d4220d4bb0b4c7
thaling <thaling@verizon.net>
2022-09-20 16:39:47 -0600

    delete route for products and README

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -128,6 +128,21 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }) .then(data => {
+    if(!data) {
+      res.status(404).json({message: 'Not found'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
